06a8c693eec7b551a65db8e9d94673752a73f311
cuongpm <pmcuong120394@gmail.com>
2017-07-14 23:50:25 +0700

    yu

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/*
+Nothing to see here :)
+Made by @screenshake
+linkedin.com/in/saranshsinha
+*/
+var played = false;
+
+function playSound(){
+    // if (played){
+    //     return;
+    // }
+    // played = true;
+    //  var snd = new Audio("./sound/MuaMuaDiQua-BaoYen-777637.mp3");
+    //     snd.play();
+    window.location.href='song.html';
+}
